abab38a068962e5db5b0decd8092fe39281432c8
tete <601517124@qq.com>
2019-12-17 14:13:16 +0800

    holmes上传到github--第一次

---
+++ src/main/java/com/hjt/holmes/HolmesApplication.java
@@ -0,0 +1,13 @@
+package com.hjt.holmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolmesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolmesApplication.class, args);
+    }
+
+}
